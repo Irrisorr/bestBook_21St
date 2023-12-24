4b61cb971b806074d63e767db28f1b1ff11d655a
Irrisorr <zakharkevichg@gmail.com>
2023-12-24 02:06:26 +0100

    Create beta-GUI and edit gitignore

---
+++ main.py
@@ -0,0 +1,12 @@
+import clips
+from tkinter import *
+
+
+if __name__ == '__main__':
+    root = Tk()
+    root.geometry('1280x800')
+    root.config(bg='#99cfbe')
+
+    root.title('Best Book of 21st')
+
+    root.mainloop()
