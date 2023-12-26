189aef0dbe8e1e5a3f971bdd75bb0d7f48b29e0b
Irrisorr <zakharkevichg@gmail.com>
2023-12-26 16:27:14 +0100

    edit desighn

--- src/main.py
+++ src/main.py
@@ -10,7 +10,6 @@ def load_files(prop):
             name, full_name = line.strip().split('=')
             prop[name] = full_name
 
-
 '''======================================= Functions to use from clips (getters) ===================================='''
 
 
@@ -36,7 +35,7 @@ def set_answers():
         if answer < len(books):
             text.set(get_properties(str(books[answer])))
             answers[answer].grid(row=answer + 1, column=0)
-            answers[answer].configure(font='Helvetica 18 bold', relief=GROOVE, padx=10, pady=10, fg='#e9736a', bd=3)
+            answers[answer].configure(font='ARIAL 25 bold', relief=RAISED, padx=10, pady=10, fg='#e9736a', bd=3)
 
 
 '''=============================== Functions to set buttons for an answering (setter)  =============================='''
@@ -48,7 +47,7 @@ def set_buttons():
     for button, text in enumerate(buttons_values):
         if button < len(answers):
             text.set(get_properties(str(answers[button])))
-            buttons[button].grid(row=button + 1, column=0)
+            buttons[button].grid(row=button//2+1, column=0, sticky=(E) if button%2 == 0 else W)
         else:
             buttons[button].grid_forget()
 
@@ -102,8 +101,8 @@ def modify_text(condition):
             for answer in answers:
                 answer.grid_forget()
 
-            question.configure(textvariable=question_text, padx=1, pady=7, bg='#F5F5DC', fg='#FFA500',
-                               font='Helvetica 12 bold', bd=1, relief=FLAT)
+            question.configure(textvariable=question_text, padx=1, pady=7, bg='#5c5c5c', fg='#FFA500',
+                               font='ARIAL 30 bold', bd=1, relief=FLAT)
             question.grid(row=0, column=0)
             indent.grid(row=len(buttons_values) + 2, column=0)
             func_button.grid(row=len(buttons_values) + 4, column=0, sticky='we')
@@ -116,9 +115,11 @@ def modify_text(condition):
 
 if __name__ == '__main__':
     root = Tk()
-    root.geometry('1280x600')
-    root.config(bg='#F5F5DC')
+    root.geometry('1200x600')
     root.title('Best book of the 21st century')
+    img = PhotoImage(file='../resources/bg.png')
+    background_label = Label(root, image=img)
+    background_label.place(relwidth=1, relheight=1)
 
     clips_env = clips.Environment()
     clips_env.load('clips.CLP')
@@ -139,35 +140,37 @@ if __name__ == '__main__':
     for i in range(20):
         text = StringVar()
         buttons_values.append(text)
-        button = Button(root, textvariable=text, width=90, padx=2, pady=2,
-                        command=lambda ii=i: button_command(ii), borderwidth=3, activebackground='#9ada7d',
-                        bg='#0099cc', fg='#0b0b0b', font='Helvetica 10 bold')
+        button = Button(root, textvariable=text, width=35, pady=10,
+                        command=lambda ii=i: button_command(ii), activebackground='#cfbb69',
+                        bg='#cf6969', fg='#0b0b0b', font='ARIAL 20 bold')
         buttons.append(button)
 
     for i in range(20):
         text = StringVar()
         answers_values.append(text)
         answer = Label(root, textvariable=text, pady=7, bg='#F5F5DC', fg='#FFA500',
-                       font='Helvetica 18 bold')
+                       font='ARIAL 18 bold')
         answers.append(answer)
 
     modify_text(True)
 
-    question = Label(root, textvariable=question_text, pady=7, bg='#F5F5DC', fg='#FFA500',
-                     font='Helvetica 12 bold')
-    indent = Label(root, text='', bg='#F5F5DC', height=1)
+    question = Label(root, textvariable=question_text, pady=7, bg='#5c5c5c', fg='#FFA500',
+                     font='ARIAL 30 bold')
+    indent = Label(root, bg='#F5F5DC', height=6)
 
     func_button = Button(root, textvariable=func_button_text, width=90, padx=2, pady=2, command=func_button_command,
-                         borderwidth=4, activebackground='#9ada7d', bg='#0099cc', fg='#0b0b0b',
-                         font='Helvetica 10 bold')
+                         borderwidth=2, activebackground='#cfbb69', bg='#5c5c5c', fg='#FFA500',
+                         font='ARIAL 15 bold')
 
     restart_button = Button(root, textvariable=restart_button_text, width=90, padx=2, pady=2, command=restart_button_command,
-                         borderwidth=4, activebackground='#9ada7d', bg='#0099cc', fg='#0b0b0b',
-                         font='Helvetica 10 bold')
+                         borderwidth=2, activebackground='#cfbb69', bg='#5c5c5c', fg='#FFA500',
+                         font='ARIAL 15 bold')
 
     question.grid(row=0, column=0)
     indent.grid(row=2, column=0)
-    func_button.grid(row=2, column=0, sticky='we')
-    restart_button.grid(row=3, column=0, sticky='we')
+    func_button.grid(row=3, column=0, sticky='we')
+    restart_button.grid(row=4, column=0, sticky='we')
+    root.grid_rowconfigure(0, weight=1)
+    root.grid_columnconfigure(0, weight=1)
 
     root.mainloop()
